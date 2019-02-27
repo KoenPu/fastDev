554711e8f9b472c1df212b440244d6dbc3f85139
pky <171663097@qq.com>
2019-02-27 19:40:13 +0800

    新建base模块

---
+++ baseModule/src/main/java/com/koen/basemodule/base/BaseActivity.java
@@ -0,0 +1,12 @@
+package com.koen.basemodule.base;
+
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
+
+/**
+ * @Description: 基类
+ * @Author: pky
+ * @CreateDate: 2019/2/27 5:14 PM
+ * @UpdateDate: 2019/2/27 5:14 PM
+ */
+public abstract class BaseActivity extends RxAppCompatActivity {
+}
---
+++ baseModule/src/main/java/com/koen/basemodule/base/BaseMVVMActivity.java
@@ -0,0 +1,12 @@
+package com.koen.basemodule.base;
+
+import android.databinding.ViewDataBinding;
+
+/**
+ * @Description: data binding的基类
+ * @Author: pky
+ * @CreateDate: 2019/2/27 7:08 PM
+ * @UpdateDate: 2019/2/27 7:08 PM
+ */
+public class BaseMVVMActivity<V extends ViewDataBinding, VM extends BaseViewModel> extends BaseActivity {
+}
